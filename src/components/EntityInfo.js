24c6416dfef0d2da650d6d2e36454a49ef71c029
Fernanda Toledo <fernandatote@gmail.com>
2018-06-01 20:19:31 -0700

    import tooltip content into into tooltip screen
    imported entity name, entity info and map into tooltip screen and connected them to the database test file.

--- src/components/EntityInfo.js
+++ src/components/EntityInfo.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-// import Db from './Db';
+import { db } from './Db';
 
 class EntityInfo extends Component {
   render(){
     var entityName = this.props.entity; //entity name or item name sent from TooltipScreen component
-
     let renderData = [];
 
-    for(let item of Db){
+    for(let item of db){
       if (item.entity === entityName){
-        renderData.push(<div>
-          <h2>Capital: {item.capital}</h2>
-          <h2>Population: {item.population}</h2>
-          <h2>Language: {item.language}</h2>
-          <h2>Greeting: {item.greeting}</h2>
-          </div>);
+        renderData.push(
+          <div>
+            <h2>Capital: {item.capital}</h2>
+            <h2>Population: {item.population}</h2>
+            <h2>Language: {item.language}</h2>
+            <h2>Greeting: {item.greeting}</h2>
+          </div>
+        );
       }
     }
     return (
@@ -23,27 +24,5 @@ class EntityInfo extends Component {
      </div>
    );
  }}
-
-//This array of data is for testing purpose
-// I made a separate file Db.js with this array,
-//but could not import it properly
-//so it is sitting here for now
-
- let Db = [
-       {
-       entity:"Japan",
-       capital: "Tokyo",
-       population: 128000000,
-       language: "Japanese",
-       greeting: "kon-ni-chiwa"
-     },
-     {
-       entity:"Canada",
-       capital: "Ottawa",
-       population: 45000000,
-       language: "English and French",
-       greeting: "Hello / Bon Jour"
-     }
- ]
 
 export default EntityInfo;
