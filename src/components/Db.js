e4bbc77ac13826a7651e34f933272934bfa28e50
Fernanda Toledo <fernandatote@gmail.com>
2018-06-01 20:16:40 -0700

    add database file to components folder

---
+++ src/components/Db.js
@@ -0,0 +1,17 @@
+export var db= [
+        {
+        entity:"Japan",
+        capital: "Tokyo",
+        population: 128000000,
+        language: "Japanese",
+        greeting: "kon-ni-chiwa"
+      },
+      {
+        entity:"Canada",
+        capital: "Ottawa",
+        population: 45000000,
+        language: "English and French",
+        greeting: "Hello / Bon Jour"
+      }
+  ]
+// };
