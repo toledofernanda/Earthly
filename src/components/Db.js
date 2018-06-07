8a8dd35a8a9db8a8d6ab29f30e2a0953d34a3dfb
Fernanda Toledo <fernandatote@gmail.com>
2018-06-07 15:46:07 -0700

    database object model to be used by components

---
+++ src/components/Db.js
@@ -0,0 +1,77 @@
+export var db = {
+  animals: {
+    most_dogs: [{
+      ranking: 1,
+      country: "United States",
+      quantity: 75800000,
+      unit: "million"
+    },{
+      ranking: 2,
+      country: "Brazil",
+      quantity: 35700000,
+      unit: "million"
+    }, {
+      ranking: 3,
+      country: "China",
+      quantity: 27400000,
+      unit: "million"
+      }
+    ],
+    most_camels: [{
+      ranking: 1,
+      country: "Somalia",
+      quantity: 7221998,
+      unit: "heads"
+    }, {
+      ranking: 2,
+      country: "Sudan",
+      quantity: 4826059,
+      unit: "heads"
+    }, {
+      ranking: 3,
+      country: "Kenya",
+      quantity: 3222593,
+      unit: "heads"
+      }
+    ]
+  },
+  general: {
+    entity_info: [{
+      entity:"Japan",
+      capital: "Tokyo",
+      population: 128000000,
+      language: "Japanese",
+      greeting: "kon-ni-chiwa"
+    }, {
+      entity:"Canada",
+      capital: "Ottawa",
+      population: 45000000,
+      language: "English and French",
+      greeting: "Hello / Bon Jour"
+    }, {
+      entity: "Brazil",
+      capital: "Brasilia",
+      population: 210750000,
+      language: "Portuguese",
+      greeting: "Olá"
+      }
+    ],
+    smallest_population: [{
+      ranking: 1,
+      country: "Vatican",
+      quantity: 809,
+      unit: "thousand"
+    }, {
+      ranking: 2,
+      country: "Niue",
+      quantity: 1600,
+      unit: "thousand"
+    }, {
+      ranking: 3,
+      country: "Tuvalu",
+      quantity: 11100,
+      unit: "thousand"
+      }
+    ]
+  }
+}
