73e9c819d4bdc25e75f1e5cf9faa63bfc49a92ee
Tomoko Okochi <tomokochi922@gmail.com>
2018-06-08 19:19:31 -0700

    ranking icon component update

---
+++ src/components/RankingIcon.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+// import icon from 'images/most_dogs.png';
+
+//This component renders icons for a ranking chart bar
+class RankingIcon extends Component{
+  render(){
+    let iconCount = this.props.iconCount; //props from RankingBar
+    let topic = this.props.topic; //props passed down
+    let image = `<img src="images/${topic}.png" alt="${topic}" />` //image tag for icon
+    let renderData = ''; //variable to hold iconCount number of icons
+
+    for(let i=0; i<iconCount; i++){
+      renderData += image;
+    }
+
+    return (
+      <div className="ranking-icon">
+        {renderData}
+      </div>
+    )
+  }
+}
+
+export default RankingIcon;
