df1863537e410bc359be73a63a43e9705c9f6874
Fernanda Toledo <fernandatote@gmail.com>
2018-06-29 19:56:41 -0700

    included logo and app name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'; //first npm install react-render-dom
 
-
 const headerDes = {
   height: '70px',
   display: 'flex',
@@ -9,9 +8,28 @@ const headerDes = {
   backgroundColor: '#19A9E1',
   marginTop: '0',
   position:'fixed',
+  zIndex: 5,
   width: '100%',
   top: '0',
   listStyleType:'none'
+}
+
+let logoName = {
+  display: 'flex',
+  alignItems: 'center'
+}
+
+let logo = {
+  height: '55px'
+}
+
+let appName = {
+  fontSize: '18pt',
+  // fontWeight: 'bold',
+  listStyleType: 'none',
+  color: 'white',
+  letterSpacing: '4px',
+  paddingLeft: '10px'
 }
 
 const button = {
@@ -28,8 +46,15 @@ class Header extends Component{
     return(
         <header className="header">
           <ul style={headerDes}>
-            <li style= {{width: '250px', marginTop:'10px'}}><Link to="/"><img src={require(`images/most_dogs.png`)} alt={'Earthly'} /></Link></li>
-            <li style= {{marginTop:'20px'}}><Link to="/donation" style={button}>Donation</Link></li>
+            <li style= {{width: '250px', marginTop:'5px'}}>
+              <Link to="/">
+                <div style={logoName}>
+                  <img src={require(`images/earthly_logo.svg`)} style={logo} alt={'Earthly'} />
+                  <span style={appName}>earthly</span>
+                </div>
+              </Link>
+            </li>
+            <li style= {{marginTop:'25px'}}><Link to="/donation" style={button}>Donation</Link></li>
           </ul>
         </header>
     )
