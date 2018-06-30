498cfaf01a08bacf386a3941977dfccff8bde4b3
manakoike <mkoike00@mylangara.bc.ca>
2018-06-29 18:26:55 -0700

    updated css

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,182 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class About extends Component {
-  render() {
-    return (
-      <div>
-        <h2>About</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
+const memberBox = {
+  fernanda: {
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent:'center',
+  alignItems:'center',
+  padding: '10px',
+  marginBottom:'60px',
+  width:'19%',
+  flex: '0 0 15%',
+  backgroundColor: '#4AB85C'
+},
 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+  mana:{
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent:'center',
+  alignItems:'center',
+  padding: '10px',
+  marginBottom:'60px',
+  width:'19%',
+  flex: '0 0 15%',
+  backgroundColor: '#F05027'
+},
+
+  tomo:{
+    color: 'white',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent:'center',
+    alignItems:'center',
+    padding: '10px',
+    marginBottom:'60px',
+    width:'19%',
+      flex: '0 0 15%',
+    backgroundColor: '#19A9E1'
+  },
+
+  jenn:{
+    color: 'white',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent:'center',
+    alignItems:'center',
+    padding: '10px',
+    marginBottom:'60px',
+    width:'19%',
+    flex: '0 0 15%',
+    backgroundColor: '#00B8A6'
+  },
+  marlowe:{
+    flasxWrap:'wrap',
+    color: 'white',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent:'center',
+    alignItems:'center',
+    padding: '10px',
+    marginBottom:'60px',
+    width:'19%',
+    flex: '0 0 15%',
+    backgroundColor: '#152D53'
+  },
+
+
+}
+
+const memberImage = {
+  marginTop: '-50px',
+  borderRadius: '50%'
+}
+
+
+class About extends Component{
+  render(){
+    return(
+      <div style={{ marginRight:'5%', marginLeft:'5%', }}>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <h1 style={{ marginBottom:'60px'}}>Meet Our Team</h1>
       </div>
-    );
+        <div style = {{display:'flex', justifyContent:'space-around', flexFlow:'row wrap', boxSizing:'border-box'}} className='aboutContainer'>
+          <div className='Fernanda' style = {memberBox.fernanda}>
+            <img style = {memberImage} img src={require(`images/most_dogs.png`)} />
+            <h3>Fernanda</h3>
+            <h4>Lead Dev</h4>
+            <p>Fernanda has a background and Advertising at Mackenzie University in
+            São Paulo, Brazil. She
+            also holds a certificate
+            in Internet and Web
+            Technology, and CSPO.
+            Fernanda is Earthly’s
+            lead Developer.</p>
+            <p>
+            Her hobbies are figure
+            skating, biking, and
+            watching TV series
+            with her cat, Garfield.
+            </p>
+
+          </div>
+          <div className='Mana' style = {memberBox.mana}>
+            <img style = {memberImage} img src={require(`images/most_dogs.png`)} />
+            <h3>Mana</h3>
+            <h4>Dev and QA</h4>
+            <p>Fernanda has a background and Advertising at Mackenzie University in
+            São Paulo, Brazil. She
+            also holds a certificate
+            in Internet and Web
+            Technology, and CSPO.
+            Fernanda is Earthly’s
+            lead Developer.</p>
+            <p>
+            Her hobbies are figure
+            skating, biking, and
+            watching TV series
+            with her cat, Garfield.
+            </p>
+          </div>
+          <div className='Tomoko' style = {memberBox.tomo}>
+            <img style = {memberImage} img src={require(`images/most_dogs.png`)} />
+            <h3>Tomoko</h3>
+            <h4>Dev and PM</h4>
+            <p>Fernanda has a background and Advertising at Mackenzie University in
+            São Paulo, Brazil. She
+            also holds a certificate
+            in Internet and Web
+            Technology, and CSPO.
+            Fernanda is Earthly’s
+            lead Developer.</p>
+            <p>
+            Her hobbies are figure
+            skating, biking, and
+            watching TV series
+            with her cat, Garfield.
+            </p>
+          </div>
+          <div className='Jenn' style = {memberBox.jenn}>
+            <img style = {memberImage} img src={require(`images/most_dogs.png`)} />
+            <h3>Jenn</h3>
+            <h4>UX and Designer</h4>
+            <p>Fernanda has a background and Advertising at Mackenzie University in
+            São Paulo, Brazil. She
+            also holds a certificate
+            in Internet and Web
+            Technology, and CSPO.
+            Fernanda is Earthly’s
+            lead Developer.</p>
+            <p>
+            Her hobbies are figure
+            skating, biking, and
+            watching TV series
+            with her cat, Garfield.
+            </p>
+          </div>
+          <div className='Marlowe' style = {memberBox.marlowe}>
+            <img style = {memberImage} img src={require(`images/most_dogs.png`)} />
+            <h3>Marlowe</h3>
+            <h4>UX and Designer</h4>
+            <p>Fernanda has a background and Advertising at Mackenzie University in
+            São Paulo, Brazil. She
+            also holds a certificate
+            in Internet and Web
+            Technology, and CSPO.
+            Fernanda is Earthly’s
+            lead Developer.</p>
+            <p>
+            Her hobbies are figure
+            skating, biking, and
+            watching TV series
+            with her cat, Garfield.
+            </p>
+          </div>
+          </div>
+      </div>
+    )
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'; //first npm install react-render-dom
 
+
+const headerDes = {
+  height: '70px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  backgroundColor: '#19A9E1',
+  marginTop: '0',
+  position:'fixed',
+  width: '100%',
+  top: '0',
+  listStyleType:'none'
+}
+
+const button = {
+  padding:'5px',
+  marginRight:'60px',
+  backgroundColor:'#F05027',
+  borderRadius: 7,
+  textDecoration:'none',
+  color:'white'
+}
+
 class Header extends Component{
   render(){
-
-    let logo = {
-      height: '55px'
-    }
-    
     return(
         <header className="header">
-          <ul>
-            <li><Link to="/"><img style={logo} src={require(`images/earthly_logo.svg`)} alt={'Earthly'} /></Link></li>
-            <li><Link to="/donation">Donation</Link></li>
+          <ul style={headerDes}>
+            <li style= {{width: '250px', marginTop:'10px'}}><Link to="/"><img src={require(`images/most_dogs.png`)} alt={'Earthly'} /></Link></li>
+            <li style= {{marginTop:'20px'}}><Link to="/donation" style={button}>Donation</Link></li>
           </ul>
         </header>
     )
   }
 }
-
 export default Header;
