456f2cf171765467823233352e9625c888f2becd
Tomoko Okochi <tomokochi922@gmail.com>
2018-07-17 22:43:32 -0700

    add ThankYou component for Contact, Contact responsiveness, mana photo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,7 @@ import {Form, FormGroup, Input, Label, Button} from 'reactstrap';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import BackButton from 'components/BackButton';
+import ThankYou from 'components/ThankYou';
 
 const boxPosition = {
   marginRight: '10vw',
@@ -44,8 +45,6 @@ const submitButton = {
 
 }
 
-
-
 class Contact extends Component {
   constructor(){
     super()
@@ -54,8 +53,8 @@ class Contact extends Component {
       lastName:'',
       email:'',
       phone:'',
-      message:''
-
+      message:'',
+      submitted: false
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -68,10 +67,12 @@ class Contact extends Component {
   }
 
   async handleSubmit(e){
-    if(!alert('Thank you for your feedback ' + this.state.firstName + '!')){window.location.reload();}
+    // if(!alert('Thank you for your feedback ' + this.state.firstName + '!')){window.location.reload();}
     //alert('Thank you for your feedback ' + this.state.firstName + '!');
 
-    e.preventDefault()
+    e.preventDefault();
+    this.setState({'submitted': true});
+
     const {firstName, lastName, email,phone, message} = this.state
 
     const form = await axios.post('/api/form',{
@@ -85,64 +86,118 @@ class Contact extends Component {
 }
 
   render() {
-    return (
-      <div style = {boxPosition} className = "donationBox" >
-        <div className = "contactTop" style = {{textAlign: 'center'}}>
-          <BackButton className='button-shadow' component={'contact'} />
-            <h1>Contact</h1>
-            <p style={{width:'50%', marginLeft:'auto', marginRight:'auto', color:'#58595B'}}>Get in touch with us.
-            We would love to hear from you What would you like to see added to Earthly?
-            Questions, comments, or complaints? Fill out the form below and we will be in touch with you shortly.
-            </p>
+    let outerDiv = {
+      maxWidth: '1024px',
+      margin: 'auto',
+      display: 'flex',
+      flexFlow: 'column wrap',
+      justifyContent: 'center',
+      marginTop: '5%',
+      marginBottom: '5%',
+      paddingBottom: '3%',
+      backgroundColor: 'white',
+      borderRadius: 30,
+    }; //style for the outside div of main component
+    let filterTopStyle = {
+      padding: '1.5% 4%',
+      display: 'grid',
+      gridTemplateColumns: '40px 10fr'
+    };
+    let TitleStyle = {
+      textAlign: 'center',
+      fontSize: '14pt',
+      flex: '0 0 100%',
+    };
+    let formStyle = {
+      display:'flex',
+      flexFlow:'row wrap',
+      justifyContent:'space-around',
+      // marginBottom: '3%',
+      // textAlign:'center',
+      color:'#58595B'
+    };
+    let inputForms = {
+      margin: '1%',
+      display: 'flex',
+      flexFlow: 'row wrap',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
+    let inputStyle = {
+      flex: '1 0 40%',
+      display:'flex',
+      justifyContent: 'space-between',
+      marginBottom:'1%'
+    }
+    let contactForm = {
+      width: '60%',
+      margin: 'auto'
+    }
+    let input = {
+      border:'1px solid gray',
+      borderRadius:7,
+      marginRight:'5%',
+    }
+    let label = {
+      marginLeft : '10%'
+    }
+
+      if (this.state.submitted){
+        return <ThankYou />;
+      }
+      else {
+        return(
+      <div style = {outerDiv} className = "donationBox" >
+        <div className="contactTitle" style={filterTopStyle}>
+          <BackButton component={'category'} />
+          <h1 style = {TitleStyle}>Contact</h1>
         </div>
-        <div className = "contactForm">
-          <Form onSubmit={this.handleSubmit} style={{display:'flex', flexWrap:'wrap', justifyContent:'center', textAlign:'center',color:'#58595B'}}>
-            <div>
-            <FormGroup  style={{display:'flex', marginRight:'30px', marginBottom:'10px'}}>
-              <Label for="firstName">First Name</Label>
-              <input style={{border:'1px solid gray', borderRadius:7, marginLeft:'5px'}}
+        <div>
+          <p style={{width:'50%', marginLeft:'auto', marginRight:'auto', color:'#58595B'}}>
+            Get in touch with Earthly's design and development team!
+          </p>
+        </div>
+        <div className = "contactForm" style = {contactForm}>
+          <Form onSubmit={this.handleSubmit} style={formStyle}>
+            <div className = "inputForms" style = {inputForms}>
+            <FormGroup  style={inputStyle}>
+              <Label for="firstName" style = {label}>First Name</Label>
+              <input style={input}
               type="text"
               name= "firstName"
               onChange = {this.handleChange}
               />
             </FormGroup>
-
-            <FormGroup style={{display:'flex',justifyContent:'flex-end', marginRight:'30px', marginBottom:'10px'}}>
-              <Label for="phone">phone</Label>
-              <input style={{border:'1px solid gray', borderRadius:7, marginLeft:'5px'}}
+            <FormGroup  style={inputStyle}>
+              <Label for="lastName" style = {label}>Last Name</Label>
+              <input style={input}
+              type="text"
+              name= "lastName"
+              onChange = {this.handleChange}
+              />
+            </FormGroup>
+          <FormGroup  style={inputStyle}>
+              <Label for="phone" style = {label}>phone</Label>
+            <input style={input}
               type="text"
               pattern="[0-9]*"
               name= "phone"
               onChange = {this.handleChange}
               />
             </FormGroup>
-
-          </div>
-          <div>
-            <FormGroup style={{display:'flex', marginRight:'30px', marginBottom:'10px'}}>
-              <Label for="lastName">Last Name</Label>
-              <input style={{border:'1px solid gray', borderRadius:7, marginLeft:'5px'}}
-              type="text"
-              name= "lastName"
-              onChange = {this.handleChange}
-              />
-            </FormGroup>
-
-            <FormGroup style={{display:'flex',justifyContent:'flex-end', marginRight:'30px', marginBottom:'10px'}}>
-              <Label for="email">Email</Label>
-              <input style={{border:'1px solid gray', borderRadius:7, marginLeft:'5px'}}
+            <FormGroup  style={inputStyle}>
+              <Label for="email" style = {label}>Email</Label>
+              <input style={input}
               type="email"
               name= "email"
               onChange = {this.handleChange}
               />
             </FormGroup>
-
-
           </div>
 
-            <FormGroup style={{flexBasis:'100%'}}>
+            <FormGroup style={{flex:'1 0 100%', textAlign:'center', marginTop:'3%', marginBottom: '3%'}}>
               <Label for="message" style={{margin:'10px'}}>Message<br /></Label>
-              <textarea style={{fontSize:'18px', padding:'5px', marginTop:'10px',marginBottom:'10px', width: '50%', height:'200px',border:'1px solid black', borderRadius:7}}
+              <textarea style={{fontSize:'18px', padding:'5px', marginTop:'10px',marginBottom:'10px', width: '100%', height:'160px',border:'1px solid black', borderRadius:7}}
               type="textarea"
               name= "message"
               onChange = {this.handleChange}
@@ -158,6 +213,7 @@ class Contact extends Component {
 
     );
   }
+}
 }
 
 export default Contact;
