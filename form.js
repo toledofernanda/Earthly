ffbd344e67bfdf32ee89e5c931271eab5650d0f2
manakoike <mkoike00@mylangara.bc.ca>
2018-07-30 22:22:41 -0700

    deleted comments

--- form.js
+++ form.js
@@ -8,12 +8,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 
-// app.set('view engine', 'ejs');
-// app.use(express.static('public'))
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-
-
-
 app.post('/api/form', (req,res) =>{
   nodemailer.createTestAccount(( err, account) => {
     const htmlEmail = `
